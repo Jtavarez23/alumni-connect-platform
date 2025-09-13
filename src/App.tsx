80ac9a400539dae7af58efc97a1142d98827df28
Jose Tavarez <jtav1219@gmail.com>
2025-09-13 02:08:04 -0400

    fix: Remove conflicting Toaster imports causing initialization error

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,6 @@
-import { Toaster } from "@/components/ui/toaster";
-import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
-import { Toaster as HotToaster } from "react-hot-toast";
 import { AuthProvider } from "@/contexts/AuthContext";
 import { RealtimeProvider } from "@/components/realtime/RealtimeProvider";
 import ErrorBoundary from "@/components/ErrorBoundary";
@@ -79,9 +76,6 @@ const App = () => (
       <AuthProvider>
         <RealtimeProvider>
             <TooltipProvider>
-          <Toaster />
-          <Sonner />
-          <HotToaster position="top-center" />
           <BrowserRouter>
             <Routes>
               <Route path="/" element={<Index />} />
